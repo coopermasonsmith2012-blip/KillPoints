f87ed9b6b15e871897cb82b6908f1bbdbf22e114
coopermasonsmith2012-blip <coopermasonsmith2012@gmail.com>
2026-02-10 14:04:04 +1300

    Initial Commit

--- src/main/java/me/killpoints/KillPointsPlugin.java
+++ src/main/java/me/killpoints/KillPointsPlugin.java
@@ -8,11 +8,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -24,65 +23,42 @@ public class KillPointsPlugin extends JavaPlugin implements Listener, CommandExe
 
     private Scoreboard scoreboard;
     private Objective pointsObjective;
-    private final String TOTEM_NAME = ChatColor.AQUA + "" + ChatColor.BOLD + "Soul Totem";
+    private Objective deathsObjective; // The new Death Counter
     
-    // Map of Player UUID -> Set of their Friends' UUIDs
-    private final Map<UUID, Set<UUID>> playerFriends = new HashMap<>();
+    private final String TOTEM_NAME = ChatColor.AQUA + "" + ChatColor.BOLD + "Soul Totem";
+    private final String MENU_TITLE = ChatColor.BLACK + "Team Management";
+
+    private final Map<UUID, TeamData> teamsByLeader = new HashMap<>();
+    private final Map<UUID, UUID> playerToTeamLeader = new HashMap<>();
 
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
-        this.getCommand("friend").setExecutor(this);
-        this.getCommand("unfriend").setExecutor(this);
+        getCommand("team").setExecutor(this);
         
+        setupScoreboard();
+        // ... (Include your addSoulTotemRecipe() call here)
+    }
+
+    private void setupScoreboard() {
         ScoreboardManager manager = Bukkit.getScoreboardManager();
         scoreboard = manager.getMainScoreboard();
 
+        // Kill Points Objective
         pointsObjective = scoreboard.getObjective("killpoints");
         if (pointsObjective == null) {
             pointsObjective = scoreboard.registerNewObjective("killpoints", Criteria.DUMMY, ChatColor.RED + "Kill Points");
             pointsObjective.setDisplaySlot(DisplaySlot.SIDEBAR);
         }
 
-        addSoulTotemRecipe();
-    }
-
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) return true;
-        Player player = (Player) sender;
-
-        if (command.getName().equalsIgnoreCase("friend")) {
-            if (args.length == 0) {
-                player.sendMessage(ChatColor.RED + "Usage: /friend <player>");
-                return true;
-            }
-            Player target = Bukkit.getPlayer(args[0]);
-            if (target == null) {
-                player.sendMessage(ChatColor.RED + "Player not found or offline.");
-                return true;
-            }
-            
-            playerFriends.computeIfAbsent(player.getUniqueId(), k -> new HashSet<>()).add(target.getUniqueId());
-            player.sendMessage(ChatColor.GREEN + "Added " + target.getName() + " to your friends list! You won't take their items or head.");
-            return true;
+        // UNIQUE DEATH COUNTER
+        deathsObjective = scoreboard.getObjective("deaths");
+        if (deathsObjective == null) {
+            // Using "deathCount" criteria makes Minecraft track it automatically, 
+            // but we use "dummy" so we can control it (e.g., ignore totem deaths)
+            deathsObjective = scoreboard.registerNewObjective("deaths", Criteria.DUMMY, ChatColor.GRAY + "Deaths");
+            deathsObjective.setDisplaySlot(DisplaySlot.BELOW_NAME); // Shows under their name in-game
         }
-
-        if (command.getName().equalsIgnoreCase("unfriend")) {
-            if (args.length == 0) {
-                player.sendMessage(ChatColor.RED + "Usage: /unfriend <player>");
-                return true;
-            }
-            
-            // Allow unfriending offline players by name
-            UUID targetId = Bukkit.getOfflinePlayer(args[0]).getUniqueId();
-            if (playerFriends.containsKey(player.getUniqueId())) {
-                playerFriends.get(player.getUniqueId()).remove(targetId);
-                player.sendMessage(ChatColor.YELLOW + "Removed " + args[0] + " from your friends list.");
-            }
-            return true;
-        }
-        return true;
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
@@ -91,7 +67,6 @@ public class KillPointsPlugin extends JavaPlugin implements Listener, CommandExe
         Player killer = victim.getKiller();
         ItemStack totemStack = null;
 
-        // Check for Soul Totem
         for (ItemStack item : victim.getInventory().getContents()) {
             if (isSoulTotem(item)) {
                 totemStack = item;
@@ -99,41 +74,46 @@ public class KillPointsPlugin extends JavaPlugin implements Listener, CommandExe
             }
         }
 
-        boolean keepInventory = false;
+        boolean protectedDeath = false;
 
+        // 1. Totem Check
         if (totemStack != null) {
-            // Totem Protection
-            keepInventory = true;
+            protectedDeath = true;
             totemStack.setAmount(totemStack.getAmount() - 1);
-            victim.sendMessage(ChatColor.LIGHT_PURPLE + "Your Soul Totem saved your items!");
-            victim.playSound(victim.getLocation(), Sound.ITEM_TOTEM_USE, 1.0f, 1.0f);
+            victim.sendMessage(ChatColor.LIGHT_PURPLE + "Soul Totem saved your life!");
         } 
+        // 2. Teammate Check
+        else if (killer != null && areTeammates(killer, victim)) {
+            protectedDeath = true;
+            victim.sendMessage(ChatColor.AQUA + "Teammate protection active!");
+        }
+        // 3. Natural Death
         else if (killer == null) {
-            // Natural Death Protection
-            keepInventory = true;
-            victim.sendMessage(ChatColor.GRAY + "Natural death. Items kept.");
-        } 
-        else if (isFriendOfKiller(killer, victim)) {
-            // FRIEND PROTECTION: Killer has victim on their friend list
-            keepInventory = true;
-            victim.sendMessage(ChatColor.AQUA + killer.getName() + " is your friend! Items kept.");
-            killer.sendMessage(ChatColor.AQUA + "You killed your friend " + victim.getName() + ". No head dropped.");
+            protectedDeath = true; // Still protected from losing items
         }
 
-        if (keepInventory) {
+        if (protectedDeath) {
             event.setKeepInventory(true);
             event.getDrops().clear();
             event.setKeepLevel(true);
             event.setDroppedExp(0);
+            // We do NOT increment death counter for protected deaths
         } else {
-            // Normal PvP Death: Drop items and drop 1 head
+            // REAL DEATH: Increment unique death counter
+            Score s = deathsObjective.getScore(victim.getName());
+            s.setScore(s.getScore() + 1);
+            
             dropSingleTrophy(victim);
+            victim.sendMessage(ChatColor.RED + "Death registered! Total deaths: " + s.getScore());
         }
     }
 
-    private boolean isFriendOfKiller(Player killer, Player victim) {
-        Set<UUID> friendsOfKiller = playerFriends.get(killer.getUniqueId());
-        return friendsOfKiller != null && friendsOfKiller.contains(victim.getUniqueId());
+    // ... (Keep your TeamData class and GUI methods from the previous step)
+
+    private boolean areTeammates(Player p1, Player p2) {
+        UUID leader1 = playerToTeamLeader.get(p1.getUniqueId());
+        UUID leader2 = playerToTeamLeader.get(p2.getUniqueId());
+        return leader1 != null && leader1.equals(leader2);
     }
 
     private void dropSingleTrophy(Player victim) {
@@ -144,42 +124,6 @@ public class KillPointsPlugin extends JavaPlugin implements Listener, CommandExe
             meta.setDisplayName(ChatColor.YELLOW + victim.getName() + "'s Trophy");
             head.setItemMeta(meta);
         }
-        head.setAmount(1);
         victim.getWorld().dropItemNaturally(victim.getLocation(), head);
-    }
-
-    private boolean isSoulTotem(ItemStack item) {
-        return item != null && item.getType() == Material.TOTEM_OF_UNDYING && 
-               item.hasItemMeta() && item.getItemMeta().getDisplayName().equals(TOTEM_NAME);
-    }
-
-    private void addSoulTotemRecipe() {
-        ItemStack soulTotem = new ItemStack(Material.TOTEM_OF_UNDYING);
-        ItemMeta meta = soulTotem.getItemMeta();
-        if (meta != null) {
-            meta.setDisplayName(TOTEM_NAME);
-            meta.setLore(Arrays.asList(ChatColor.GRAY + "Prevents loss in PvP.", ChatColor.DARK_PURPLE + "Stackable."));
-            soulTotem.setItemMeta(meta);
-        }
-        ShapedRecipe recipe = new ShapedRecipe(new NamespacedKey(this, "soul_totem"), soulTotem);
-        recipe.shape("DED", "ETE", "DED");
-        recipe.setIngredient('D', Material.DIAMOND_BLOCK);
-        recipe.setIngredient('E', Material.EMERALD_BLOCK);
-        recipe.setIngredient('T', Material.TOTEM_OF_UNDYING);
-        Bukkit.addRecipe(recipe);
-    }
-
-    @EventHandler
-    public void onUseHead(PlayerInteractEvent event) {
-        if (event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
-        ItemStack item = event.getItem();
-        if (item == null || item.getType() != Material.PLAYER_HEAD) return;
-        
-        Player p = event.getPlayer();
-        Score s = pointsObjective.getScore(p.getName());
-        s.setScore(s.getScore() + 1);
-        item.setAmount(item.getAmount() - 1);
-        p.sendMessage(ChatColor.GREEN + "Trophy Redeemed! +1 Point");
-        p.playSound(p.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0f, 1.0f);
     }
 }
